286d476e6ee5d165d695a8ae5668d3b9c7964052
Krzysztof Tomaszewski <krzysztof.tom@outlook.com>
2023-09-12 21:16:57 +0200

    initial commit

---
+++ src/main/java/pl/company/foodatu/FoodatuApplication.java
@@ -0,0 +1,13 @@
+package pl.company.foodatu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodatuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodatuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/company/foodatu/FoodatuApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.company.foodatu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodatuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7187c6e42075e9bdb79e61556718fadb48544ced
Krzysztof Tomaszewski <krzysztof.tom@outlook.com>
2023-10-29 20:56:33 +0100

    Added testcontainers and separate profiles configuration

--- src/main/java/pl/company/foodatu/plans/dto/PlanResponse.java
+++ src/main/java/pl/company/foodatu/plans/dto/PlanResponse.java
@@ -1,5 +1,7 @@
 package pl.company.foodatu.plans.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.util.List;
 
 public record PlanResponse(
@@ -11,6 +13,7 @@ public record PlanResponse(
         return new PlanResponse(List.of(), 0.0);
     }
 
+    @JsonProperty("kCal")
     public Double getKCal() {
         return kCal;
     }
--- src/test/java/pl/company/foodatu/FoodatuApplicationTests.java
+++ src/test/java/pl/company/foodatu/FoodatuApplicationTests.java
@@ -1,11 +1,16 @@
 package pl.company.foodatu;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.testcontainers.containers.PostgreSQLContainer;
 import pl.company.foodatu.plans.PlansClient;
 import pl.company.foodatu.plans.dto.PlanResponse;
 
@@ -22,6 +27,27 @@ class FoodatuApplicationTests {
 
 	@Autowired
 	PlansClient plansClient;
+
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+			"postgres:15-alpine"
+	);
+
+	@BeforeAll
+	static void beforeAll() {
+		postgres.start();
+	}
+
+	@AfterAll
+	static void afterAll() {
+		postgres.stop();
+	}
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	@Test
 	void addedMeal_then_planContains1MealAndItIsReturnedWithPlan() {

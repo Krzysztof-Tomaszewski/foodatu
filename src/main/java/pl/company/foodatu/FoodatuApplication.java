343d1b702314547c6a2a841aa3c89100b81ab4d2
Krzysztof Tomaszewski <krzysztof.tom@outlook.com>
2023-12-14 23:14:34 +0100

    Added annotation to enable Mongo repositories

--- src/main/java/pl/company/foodatu/FoodatuApplication.java
+++ src/main/java/pl/company/foodatu/FoodatuApplication.java
@@ -2,8 +2,10 @@ package pl.company.foodatu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "pl.company.foodatu")
 public class FoodatuApplication {
 
 	public static void main(String[] args) {

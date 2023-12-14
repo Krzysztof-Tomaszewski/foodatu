48fb920d72896e76bec7c3328178cc31bab55682
Krzysztof Tomaszewski <krzysztof.tom@outlook.com>
2023-12-14 23:26:06 +0100

    Added prod profile to separate configurations

--- src/main/java/pl/company/foodatu/FoodatuApplication.java
+++ src/main/java/pl/company/foodatu/FoodatuApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-@EnableMongoRepositories(basePackages = "pl.company.foodatu")
 public class FoodatuApplication {
 
 	public static void main(String[] args) {
